1a018d29450b17be88f5a998fbada0dd468ed975
Athaillahrafi <mrafi0502@gmail.com>
2025-07-18 23:50:29 +0700

    Welcome pop up

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log('javascript is working!');
+
+// Fungsi untuk memanggil welcome pop up
+
+// Fungsi untuk menampilkan welcome pop up
+function showWelcomePopUp() {
+    let userName = prompt('Please Enter Your Name:');
+    if (userName != '') {
+        document.getElementById('welcome-user').innerHTML = userName;
+    }
+}
